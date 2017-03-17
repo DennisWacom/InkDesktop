10dcfd420a27ef1b46a029180232af56bd8a9902
Dennis Cai <dennis.cai@wacom.com.sg>
2017-03-17 13:01:47 +0800

    Added missing SlideShowSettings.cs

---
+++ InkDesktop/SlideShowSettings.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.Serialization;
+
+namespace InkDesktop
+{
+    [DataContract]
+    public class SlideShowSettings
+    {
+        [DataMember]
+        public ProductSlideShowSettings[] Items;
+    }
+
+    [DataContract]
+    public class ProductSlideShowSettings
+    {
+        [DataMember]
+        public string Vendor;
+
+        [DataMember]
+        public string ProductModel;
+
+        [DataMember]
+        public ushort Vid;
+
+        [DataMember]
+        public ushort Pid;
+
+        [DataMember]
+        public string[] ImageList;
+
+        [DataMember]
+        public int Interval; 
+    }
+}
